10cd2e00a0bd40ccc63eea5d75b702486f43714d
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-24 13:27:15 -0400

    Update manager recursive.js
    Revised code to show hierarchical manager structure. Code has an error in it that doesn't return the correct structure.

--- manager recursive.js
+++ manager recursive.js
@@ -1,7 +1,6 @@
-function managerRecurse(userSysID, outputArr) {
+function managerRecurse(userSysID, outputArr, counter) {
   var grSysUser = new GlideRecord('sys_user');
   grSysUser.get(userSysID);
-
   var usersManagerID = grSysUser.getValue('manager');
   if (usersManagerID === null) {
     return outputArr;
@@ -10,13 +9,33 @@ function managerRecurse(userSysID, outputArr) {
   var userFullName = grSysUser.getDisplayValue('name');
   var usersManagerName = grSysUser.getDisplayValue('manager');
 
-  outputArr.push({employee:userFullName, userManager:{name:usersManagerName}});
+  if (counter === 0) {
 
-  return managerRecurse(usersManagerID,outputArr);
+    outputArr.push({
+      "employee": {
+        "name": userFullName,
+        "manager": {
+          "name": usersManagerName
+        }
+      }
+    })
+  } else {
+
+    var managerObject = 'outputArr[0]["employee"]["manager"]';
+
+    function addManager(managerName, managerObject) {
+      var newManagerObject = managerObject + '["manager"]';
+      eval(newManagerObject + ' = {"name": "' + managerName + '"}');
+      return newManagerObject;
+    }
+    managerObject = addManager(userFullName, managerObject);
+
+  }
+  return managerRecurse(usersManagerID, outputArr, counter + 1);
 
 }
 
 
 
-var resultArr = managerRecurse('02826bf03710200044e0bfc8bcbe5d88',[]);
+var resultArr = managerRecurse('02826bf03710200044e0bfc8bcbe5d88', [],0);
 gs.info(JSON.stringify(resultArr));

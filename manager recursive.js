85a6e173cf733e3ccd755573ed604e70fcce8388
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-24 13:29:43 -0400

    Update manager recursive.js
    Cleaned up the code. The main logic is now wrapped in a function and only requires the Syd id of the user to be passed.

--- manager recursive.js
+++ manager recursive.js
@@ -1,36 +1,36 @@
-var objectString = 'outputArr[0]["employee"]["manager"]';
+userManagers('02826bf03710200044e0bfc8bcbe5d88');
 
-function managerRecurse(userSysID, outputArr, counter, objectString) {
-  var grSysUser = new GlideRecord('sys_user');
-  grSysUser.get(userSysID);
-  var usersManagerID = grSysUser.getValue('manager');
-  if (usersManagerID === null) {
-    return outputArr;
-  }
+function userManagers(userSysID) {
+  var objectString = 'outputArr[0]["employee"]["manager"]';
 
-  var userFullName = grSysUser.getDisplayValue('name');
-  var usersManagerName = grSysUser.getDisplayValue('manager');
+  function managerRecurse(userSysID, outputArr, counter, objectString) {
+    var grSysUser = new GlideRecord('sys_user');
+    grSysUser.get(userSysID);
+    var usersManagerID = grSysUser.getValue('manager');
+    if (usersManagerID === null) {
+      return outputArr;
+    }
 
-  if (counter === 0) {
+    var userFullName = grSysUser.getDisplayValue('name');
+    var usersManagerName = grSysUser.getDisplayValue('manager');
 
-    outputArr.push({
-      "employee": {
-        "name": userFullName,
-        "manager": {
-          "name": usersManagerName
+    if (counter === 0) {
+
+      outputArr.push({
+        "employee": {
+          "name": userFullName,
+          "manager": {
+            "name": usersManagerName
+          }
         }
-      }
-    });
-  } else {
-    objectString += '["manager"]'
-    eval(objectString + ' = {"name": "' + usersManagerName + '"}');
+      });
+    } else {
+      objectString += '["manager"]'
+      eval(objectString + ' = {"name": "' + usersManagerName + '"}');
 
-  }
-  return managerRecurse(usersManagerID, outputArr, counter + 1, objectString);
+    }
+    return managerRecurse(usersManagerID, outputArr, counter + 1, objectString);
 
+  };
+  return gs.info(JSON.stringify(managerRecurse(userSysID, [], 0, objectString)));
 };
-
-
-
-var resultArr = managerRecurse('02826bf03710200044e0bfc8bcbe5d88', [], 0, objectString);
-gs.info(JSON.stringify(resultArr));

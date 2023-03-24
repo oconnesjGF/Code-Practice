fbdc6c47f21f559a607e4c0805f7307a3facf0ba
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-24 15:29:55 -0400

    Update userArray manager recurision.js
    Add notes

--- userArray manager recurision.js
+++ userArray manager recurision.js
@@ -1,53 +1,57 @@
-var grSysUser = new GlideRecord('sys_user');
-grSysUser.addEncodedQuery("active=true^managerISNOTEMPTY");
-grSysUser.query();
-var userSysIDArr = [];
-while (grSysUser.next()) {
-    userSysIDArr.push(grSysUser.getValue('sys_id'));
+function getUserRecords() {
+    /*This function gets all active users that have a manager and returns an array of sys ID. Then its gets the length of the array - 1. The return statement will call the recursive function to go through each sys id */
 
-}
-var userIDArrLength = userSysIDArr.length - 1;
-
-userSysIDLoop(userSysIDArr, 0); // this calls the function and passes the sysd id array and the starting point 
-
-function userSysIDLoop(userSysIDArr, counter) {
-
-    if (counter === userIDArrLength) {
-        return;
-    }
-    var userSysID = userSysIDArr[counter];
-
-    var objectString = 'outputArr[' + counter + ']["employee"]["manager"]';
-    //gs.info(userSysID);
-
-
-    //before userSysIDLoop increments it needs to pass in the current sys id to the manager recurse function 
     var grSysUser = new GlideRecord('sys_user');
-    grSysUser.get(userSysID);
-    var usersManagerID = grSysUser.getValue('manager');
+    grSysUser.addEncodedQuery("active=true^managerISNOTEMPTY");
+    grSysUser.query();
+    var userSysIDArr = [];
+    while (grSysUser.next()) {
+        userSysIDArr.push(grSysUser.getValue('sys_id'));
 
-    if (usersManagerID === null) {
-        return outputArr;
     }
-    var userFullName = grSysUser.getDisplayValue('name');
-    var usersManagerName = grSysUser.getDisplayValue('manager');
+    var userIDArrLength = userSysIDArr.length - 1;
+    //function ends
 
-    if (counter === 0) {
 
-        outputArr.push({
-            "employee": {
-                "name": userFullName,
-                "manager": {
-                    "name": usersManagerName
+    function userSysIDLoop(userSysIDArr, userIDArrLength) {
+        /*This function will recursively increment through the user Sys ID array and pass each sys id into the next function */
+        var objectString = 'outputArr[' + counter + ']["employee"]["manager"]';
+
+        if (counter === userIDArrLength) {
+            return;
+        }
+        var userSysID = userSysIDArr[counter];
+
+
+
+        grSysUser = new GlideRecord('sys_user');
+        grSysUser.get(userSysID);
+        var usersManagerID = grSysUser.getValue('manager');
+
+        if (usersManagerID === null) {
+            return outputArr;
+        }
+        var userFullName = grSysUser.getDisplayValue('name');
+        var usersManagerName = grSysUser.getDisplayValue('manager');
+
+        if (counter === 0) {
+
+            outputArr.push({
+                "employee": {
+                    "name": userFullName,
+                    "manager": {
+                        "name": usersManagerName
+                    }
                 }
-            }
-        });
-    } else {
-        objectString += '["manager"]'
-        eval(objectString + ' = {"name": "' + usersManagerName + '"}');
+            });
+        } else {
+            objectString += '["manager"]'
+            eval(objectString + ' = {"name": "' + usersManagerName + '"}');
+
+        }
+
 
     }
-    return userSysIDLoop(userSysIDArr, counter + 1)
-
+    return userSysIDLoop(userSysIDArr, userIDArrLength);
 
 }

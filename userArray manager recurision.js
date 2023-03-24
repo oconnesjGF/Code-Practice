eb73a567f4bba82d2788ce0a5b9dc73164101e37
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-24 15:03:40 -0400

    Update userArray manager recurision.js
    removed nested function

--- userArray manager recurision.js
+++ userArray manager recurision.js
@@ -6,9 +6,9 @@ while (grSysUser.next()) {
     userSysIDArr.push(grSysUser.getValue('sys_id'));
 
 }
-
 var userIDArrLength = userSysIDArr.length - 1;
 
+userSysIDLoop(userSysIDArr, 0); // this calls the function and passes the sysd id array and the starting point 
 
 function userSysIDLoop(userSysIDArr, counter) {
 
@@ -18,11 +18,36 @@ function userSysIDLoop(userSysIDArr, counter) {
     var userSysID = userSysIDArr[counter];
 
     var objectString = 'outputArr[' + counter + ']["employee"]["manager"]';
-    gs.info(userSysID);
-    userSysIDLoop(userSysIDArr, counter + 1)
+    //gs.info(userSysID);
 
 
-    
+    //before userSysIDLoop increments it needs to pass in the current sys id to the manager recurse function 
+    var grSysUser = new GlideRecord('sys_user');
+    grSysUser.get(userSysID);
+    var usersManagerID = grSysUser.getValue('manager');
+
+    if (usersManagerID === null) {
+        return outputArr;
+    }
+    var userFullName = grSysUser.getDisplayValue('name');
+    var usersManagerName = grSysUser.getDisplayValue('manager');
+
+    if (counter === 0) {
+
+        outputArr.push({
+            "employee": {
+                "name": userFullName,
+                "manager": {
+                    "name": usersManagerName
+                }
+            }
+        });
+    } else {
+        objectString += '["manager"]'
+        eval(objectString + ' = {"name": "' + usersManagerName + '"}');
+
+    }
+    return userSysIDLoop(userSysIDArr, counter + 1)
+
+
 }
-userSysIDLoop(userSysIDArr, 0)
-

427e6863a50999a4211cb79a5067e5c9b07ff349
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-28 08:26:58 -0400

    Update Top-down hierarchy.js

--- Top-down hierarchy/Top-down hierarchy.js
+++ Top-down hierarchy/Top-down hierarchy.js
@@ -1,47 +1,44 @@
 function stringify(string) {
-    gs.info(JSON.stringify(string));
-  }
-  
-  function getUserInfo(sys_id, value) {
-    var sysUserGR = new GlideRecord("sys_user");
-    sysUserGR.get(sys_id);
-    return sysUserGR.getValue(value);
-  }
-  
-  function getUsersWithManagersRecords() {
-    var sysUserGR = new GlideRecord("sys_user");
-    sysUserGR.addEncodedQuery("active=true^managerISNOTEMPTY");
-    sysUserGR.query();
-    var userSysIDArr = [];
-  
-    while (sysUserGR.next()) {
-      userSysIDArr.push({
-        sys_id: sysUserGR.getValue("sys_id"),
-        name: sysUserGR.getValue("name"),
-        managerID: getUserInfo(sysUserGR.manager.getValue("sys_id"), "sys_id"),
-      //  managerName: getUserInfo(sysUserGR.manager.getValue("sys_id"), "name"),
-      });
+  gs.info(JSON.stringify(string));
+}
+
+
+function getUsersWithManagersRecords() {
+  //getUsersWithManagersRecords() gets all records where users have managers
+  var sysUserGR = new GlideRecord("sys_user");
+  sysUserGR.addEncodedQuery("active=true^managerISNOTEMPTY");
+  sysUserGR.query();
+  var userSysIDArr = [];
+
+  while (sysUserGR.next()) {
+    userSysIDArr.push(sysUserGR.getValue("sys_id"))
+  };
+//while loop pushes sys ids into array
+  function userSysIDLoop(counterA, userSysIDArr) {
+    var userIDArrLength = userSysIDArr.length - 1;
+    if (counterA === userIDArrLength) {
+      return outputArr;
     }
-  
-    function getManagers(userSysIDArr, counter) {
-     // var userArrLength = userSysIDArr.length - 1;
+    //userSysIDLoop steps through each id in the array and passes it into the next function
+    var userSysID = userSysIDArr[counterA];
+
+    function managerRecurse(userSysID, outputArr) {
+
+      var sysUser = new GlideRecord('sys_user');
+      sysUser.get(userSysID);
+      var usersManagerID = sysUser.getValue('manager');
+      var userFullName = sysUser.getDisplayValue('name');
+      var usersManagerName = sysUser.getDisplayValue('manager');
       
-      var currentManagerID = userSysIDArr[counter].managerID;
-      if(getUserInfo(currentManagerID, "manager") === "null"){
-          return getManagers(userSysIDArr, counter+1);
+      if (usersManagerID === null) {
+        return userSysIDLoop(counterA + 1, userSysIDArr);
       }
-  
-      userSysIDArr.push({
-          sys_id: getUserInfo(currentManagerID, "sys_id"),
-          name: getUserInfo(currentManagerID, "name"),
-          managerID: getUserInfo(currentManagerID, "manager"),
-        //  managerName: getUserInfo(currentManagerID, "manager").name,
-        });
-  
-        return userSysIDArr;
+
+
+
+
     }
-    return getManagers(userSysIDArr, 0);
   }
-  
-  stringify(getUsersWithManagersRecords());
-  
+}
+
+stringify(getUsersWithManagersRecords());

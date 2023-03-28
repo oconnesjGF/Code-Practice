d6c93d856bdb5b0a057cf4416740d5ea7ac8ca7e
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-27 20:45:45 -0400

    Update Top-down hierarchy.js
    rebuilding entire script

--- Top-down hierarchy/Top-down hierarchy.js
+++ Top-down hierarchy/Top-down hierarchy.js
@@ -11,70 +11,7 @@ function getUsersWithManagersRecords() {
         userSysIDArr.push(sysUserGR.getValue('sys_id'));
 
     }
-    var topLevelManagersArr = [];
     
-    function passEachID(counterA, userSysIDArr, topLevelManagersArr) {
-        userArrLength = userSysIDArr.length - 1;
-        if (counterA === userArrLength) {
-            return topLevelManagersArr;
-        }
-
-        var userSysID = userSysIDArr[counterA];
-
-        function userRecurse(userSysID, topLevelManagersArr) {
-            var sysUserGR = new GlideRecord('sys_user');
-            sysUserGR.get(userSysID);
-            var currentUsersManagerID = sysUserGR.getValue('manager');
-
-            if (currentUsersManagerID != null) {
-                return managerRecurseCheck(sysUserGR, currentUsersManagerID, topLevelManagersArr);
-            }
-
-            function managerRecurseCheck(sysUserGR, currentUsersManagerID, topLevelManagersArr) {
-                var currentUserID = currentUsersManagerID;
-                sysUserGR.get(currentUserID);
-                currentUsersManagerID = sysUserGR.getValue('manager');
-                if (currentUsersManagerID === null) {
-                    if (topLevelManagersArr.indexOf(currentUserID) === -1) {
-                        topLevelManagersArr.push(currentUserID);
-                    }
-                    return passEachID(counterA + 1, userSysIDArr, topLevelManagersArr);
-                }
-
-                return managerRecurseCheck(sysUserGR, currentUsersManagerID, topLevelManagersArr)
-            }
-        }
-        return userRecurse(userSysID, topLevelManagersArr);
-    }
-    return passEachID(0, userSysIDArr, topLevelManagersArr);
 }
 
 
-
-// topLevelManagersArr gets the top most users that dosent have a manager and pushes into an array 
-var topLevelManagersArr = getUsersWithManagersRecords();
-function getName(userID) {
-    var sysUserGR = new GlideRecord('sys_user');
-    sysUserGR.get(userID);
-    return sysUserGR.getValue('name');
-}
-
-function reportsTo(counter, topLevelManagersArr, managerArr) {
-    managerArrLength = topLevelManagersArr.length;
-    var topMangerID = topLevelManagersArr[counter];
-    //this functions goes through each id in the array and passes into the next function 
-
-    if (counter === managerArrLength) {
-        return managerArr;
-    }
-
-    managerArr.push({
-        "manager_ID": topMangerID,
-        "employee": getName(topMangerID),
-    });
-
-    return reportsTo(counter+1,topLevelManagersArr, managerArr)
-
-}
-//gs.info(JSON.stringify(reportsTo(0, topLevelManagersArr, [])[0]['direct reports'][0]))
-stringify(reportsTo(0, topLevelManagersArr, []))

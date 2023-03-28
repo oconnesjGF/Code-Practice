eb7da09fadfd974f621b52effffc7ac1baa7ef8c
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-27 21:05:07 -0400

    update

--- Top-down hierarchy/Top-down hierarchy.js
+++ Top-down hierarchy/Top-down hierarchy.js
@@ -1,17 +1,37 @@
 function stringify(string) {
-    gs.info(JSON.stringify(string));
+  gs.info(JSON.stringify(string));
+}
+
+function getUserInfo(sysID, value){
+    var sysUserGR = new GlideRecord("sys_user");
+
 }
 
 function getUsersWithManagersRecords() {
-    var sysUserGR = new GlideRecord('sys_user');
-    sysUserGR.addEncodedQuery("active=true^managerISNOTEMPTY");
-    sysUserGR.query();
-    var userSysIDArr = [];
-    while (sysUserGR.next()) {
-        userSysIDArr.push(sysUserGR.getValue('sys_id'));
+  var sysUserGR = new GlideRecord("sys_user");
+  sysUserGR.addEncodedQuery("active=true^managerISNOTEMPTY");
+  sysUserGR.query();
+  var userSysIDArr = [];
 
+
+  while (sysUserGR.next()) {
+    userSysIDArr.push({
+        sys_id: 
+    });
+  }
+
+  function passEachID(counterA, userSysIDArr, topLevelManagersArr) {
+    userArrLength = userSysIDArr.length - 1;
+    if (counterA === userArrLength) {
+      return topLevelManagersArr;
     }
-    
+
+    var userSysID = userSysIDArr[counterA];
+
+    function userRecurse(userSysID, topLevelManagersArr) {
+      var sysUserGR = new GlideRecord("sys_user");
+      sysUserGR.get(userSysID);
+      var currentUsersManagerID = sysUserGR.getValue("manager");
+    }
+  }
 }
-
-

036be7957d5fd0317e068b96f0fe98477aa7472c
oconnesjGF <samuel.oconnell@glidefast.com>
2023-03-29 10:05:30 -0400

    Update Top-down hierarchy.js
    Renamed an variable for clarity and add comments.

--- Top-down hierarchy/Top-down hierarchy.js
+++ Top-down hierarchy/Top-down hierarchy.js
@@ -25,7 +25,6 @@ function getUsersWithManagersRecords() {
     /*userSysIDLoop recursively gets each sys id in the array
      and passes it in the next function.*/
 
-
     function managerRecurse(userSysID, objectArr) {
 
       var sysUser = new GlideRecord('sys_user');
@@ -42,9 +41,7 @@ function getUsersWithManagersRecords() {
           managerName: null
         })
         return userSysIDLoop(counterA + 1, userSysIDArr, objectArr);
-
       }
-
       objectArr.push({
         sys_id: userSysID,
         name: userFullName,
@@ -59,6 +56,7 @@ function getUsersWithManagersRecords() {
     return managerRecurse(userSysID, objectArr);
   }
   return (userSysIDLoop(0, userSysIDArr, []));
+  // This is the end of the first recursion loop
 }
 
 var objectArr = getUsersWithManagersRecords();
@@ -66,32 +64,40 @@ var objectArr = getUsersWithManagersRecords();
 
 function checkValues(objectArr, counterB, counterC) {
   var objectArrLength = objectArr.length;
-  var userSysID = objectArr[counterB].managerID;
+  var currentManagerSysID = objectArr[counterB].managerID;
   var sysUser = new GlideRecord('sys_user');
-  sysUser.get(userSysID);
+  sysUser.get(currentManagerSysID);
   var usersManagerID = sysUser.getValue('manager');
   var userFullName = sysUser.getDisplayValue('name');
   var usersManagerName = sysUser.getDisplayValue('manager');
+
+  /*
+    The main logic of this function works by first passing in a managerID from the array
+    and checking if the mangerID equals the sys_id property in any 
+    of the objects in the array. If a match is found, the function is called again 
+    with the next object in the array. If no match is found, the function is called again
+    with counterC incremented. Once counterC is equal to the length of the objectArr 
+    it will do an additional check to see if the currentManagerSysID value is null 
+    to prevent empty records from being pushed. It will then check if the current sysID has a manager or not. 
+    Once the function has gone through each managerID in the array, it will stop the recursion and return the results.
+  */
 
   if (counterB === objectArrLength) {
     return objectArr;
   }
 
   if (counterC === objectArrLength) {
-    //testArr.push(objectArr[counterB].managerID);
-    if (userSysID != null) {
-
+    if (currentManagerSysID != null) {
       if (usersManagerID === null) {
         objectArr.push({
-          sys_id: userSysID,
+          sys_id: currentManagerSysID,
           name: userFullName,
           managerID: null,
           managerName: null
         })
       } else {
-
         objectArr.push({
-          sys_id: userSysID,
+          sys_id: currentManagerSysID,
           name: userFullName,
           managerID: usersManagerID,
           managerName: usersManagerName
@@ -105,20 +111,26 @@ function checkValues(objectArr, counterB, counterC) {
     return checkValues(objectArr, counterB + 1, 0)
   }
   return checkValues(objectArr, counterB, counterC + 1)
-
 }
+
 var unsortedArr = checkValues(objectArr, 0, 0);
 
 
-var groupedArr = unsortedArr.reduce(function(result, current) {
+var groupedArr = unsortedArr.reduce(function (result, current) {
   if (current.managerName !== null) {
     if (!result[current.managerName]) {
-      result[current.managerName] = { direct_reports: [] };
+      result[current.managerName] = {
+        direct_reports: []
+      };
     }
-    result[current.managerName].direct_reports.push({employee:current.name});
+    result[current.managerName].direct_reports.push({
+      name: current.name
+    });
   }
   return result;
 }, {});
 
-groupedArr = { "Employee Hierarchy" : groupedArr };
+groupedArr = {
+  "Employee Hierarchy": groupedArr
+};
 stringifyOutput(groupedArr);
